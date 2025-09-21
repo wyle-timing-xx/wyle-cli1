a5b59e8d0105b3390cae176eaf65a9c115a0b0be
rexingleung <rexingleung@126.com>
2025-09-22 07:08:53 +0800

    feat: adding version

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,38 @@
 #!/usr/bin/env node
 
-const commander = require('commander');
+const { program } = require('commander');
+const { showVersion } = require('../lib/version');
+const pkg = require('../package.json');
 
-commander
-  .version(require('../package.json').version)
-  .description('Wyle CLI')
-  .option('-v, --version', 'output the version number')
-  .option('-h, --help', 'output usage information')
-  .parse(process.argv);
+// 检查是否是版本参数
+const args = process.argv.slice(2);
+if (args.includes('-v') || args.includes('--version')) {
+  showVersion();
+  process.exit(0);
+}
+
+// 配置 Commander
+program
+  .name('wyle-gen')
+  .description(pkg.description)
+  .helpOption('-h, --help', '显示帮助信息');
+
+// 主要命令
+program
+  .command('create <project-name>')
+  .description('创建新的前端项目')
+  .option('-t, --template <template>', '选择模板类型', 'react')
+  .action((projectName, options) => {
+    console.log(`创建项目: ${projectName}`);
+    console.log(`模板: ${options.template}`);
+    // 这里调用你的创建项目逻辑
+    // require('../lib/create')(projectName, options);
+  });
+
+// 解析命令
+program.parse();
+
+// 如果没有参数，显示帮助
+if (!process.argv.slice(2).length) {
+  program.help();
+}
